6ac4de11c9d176270bc6283e656348065ae6c7f4
Jason Chuy <jachuy0@gmail.com>
2024-08-15 16:53:41 -0500

    Styling site
    Worked on styling the navbar, home page, and projects page.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  // styles: ` * { border: 2px solid black }`
+  // styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '88': '22rem',
+      }
+    },
   },
   plugins: [],
 }
